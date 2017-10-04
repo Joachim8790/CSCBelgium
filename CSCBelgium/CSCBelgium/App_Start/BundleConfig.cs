15ff8de282a4b4de9e2b7f78b7564237c929ba76
joach <joach@DESKTOP-M5K8GTK>
2017-10-04 21:07:01 +0200

    comment bootstrap js out

--- CSCBelgium/CSCBelgium/App_Start/BundleConfig.cs
+++ CSCBelgium/CSCBelgium/App_Start/BundleConfig.cs
@@ -27,7 +27,7 @@ namespace CSCBelgium
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                     // "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
